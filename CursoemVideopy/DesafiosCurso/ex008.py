b6828e43707e3514ce38c01adf37095acd439cbd
Gustavo <155028443+4sofa@users.noreply.github.com>
2024-06-23 15:15:35 -0300

    Delete CursoemVideopy/DesafiosCurso/ex008.py

--- CursoemVideopy/DesafiosCurso/ex008.py
+++
@@ -1,4 +0,0 @@
-m = int(input('Quantos metros é da sua casa até a escola? '))
-c = m / 0.01
-mm = c / 0.10
-print('A distancia em centimetros é: {}. \nja a distancia em milimetros é: {}'.format(c, mm))
