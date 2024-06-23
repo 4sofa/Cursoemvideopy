6d89ee00a479bb88624789eede1373ff4568c09d
Gustavo <155028443+4sofa@users.noreply.github.com>
2024-06-23 15:15:46 -0300

    Add files via upload

---
+++ CursoemVideopy/DesafiosCurso/ex008.py
@@ -0,0 +1,8 @@
+m = float(input('Quantos metros é da sua casa até a escola? '))
+km = m * 0.001
+hm = m * 0.01
+dm = m * 0.1
+c = m * 100
+mm = m * 1000
+print('Em Kilometros: {}\nEm hectometros: {}\nEm decimetros: {}\nEm centimetros: {} \nEm milimetros: {}'
+      .format(km, hm, dm, c, mm))
